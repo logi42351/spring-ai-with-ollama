dad210673559ad87bd6b835924b03a8d4d916bfb
Logesh R <logi42351@gmail.com>
2026-03-28 23:41:43 +0530

    Ollama Options,ChatModel bean creation and usage of chat roles in messages

---
+++ src/main/java/com/aiservice/backend/config/OllamaConfig.java
@@ -0,0 +1,72 @@
+package com.aiservice.backend.config;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.ai.ollama.api.OllamaApi;
+import org.springframework.ai.ollama.api.OllamaChatOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OllamaConfig {
+
+
+    @Bean
+    ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
+
+        /**
+         * Use of chat Roles , to provide better context understanding
+         * limiting the model's scope to respond user's query
+
+         * System -> setting the context/ What this model is capable of/ what is the task assigned to this model.
+         * User -> user prompts
+         * Assistant -> Response given by Model
+         *
+         * All these are Objects of type of Prompt -> User/System/Assistant/Tool message  ->Messages
+         */
+
+        return chatClientBuilder
+                .defaultSystem("""
+                        You are a Senior Developer expertise is SpringBoot,
+                        If you asks questions related to anything else, Instruct them clearly that you are not designed for it.
+                        """)
+                .defaultUser("How can you you help me?")
+                .build();
+    }
+
+    @Bean
+    OllamaChatOptions chatOptions(){
+        return OllamaChatOptions.builder()
+                .temperature(0.5)
+                .model("llama3.2")
+                .maxTokens(1000)
+                .build();
+    }
+
+    /**
+     * Chat Model bean needs OllamaApi (http://localhost:11343) bean.
+     * OllamaOptions bean (configure temperature) , most of these beans are
+     * created by Spring itself ,using ConditionalOnMissingBean annotations
+     */
+
+    /**
+     * When we use ollama-ai package, ChatModel bean is configured by default with Mistral model
+     * our Task is to use ChatClient bean to interact with the llm model.
+     */
+
+//    @Bean
+//    OllamaApi ollamaApi(){
+//        return OllamaApi.builder()
+//                .baseUrl("http://localhost:11434")
+//                .build();
+//    }
+
+    @Bean
+    OllamaChatModel ollamaChatModel(OllamaApi ollamaApi, OllamaChatOptions ollamaChatOptions){
+        return OllamaChatModel.builder()
+                .defaultOptions(ollamaChatOptions)
+                .ollamaApi(ollamaApi)
+                .build();
+    }
+
+}
--- src/main/java/com/aiservice/backend/controller/ChatController.java
+++ src/main/java/com/aiservice/backend/controller/ChatController.java
@@ -1,10 +1,12 @@
 package com.aiservice.backend.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("/api")
@@ -12,33 +14,20 @@ public class ChatController {
 
     private final ChatClient chatClient;
 
-    public ChatController(ChatClient.Builder chatClient) {
-        this.chatClient = chatClient
-                .build();
+    public ChatController(ChatClient chatClient) {
+        this.chatClient = chatClient ;
     }
-
-    /**
-     * Chat Model bean needs OllamaApi (http://localhost:11343) bean.
-     * OllamaOptions bean (configure temperature) , most of these beans are
-     * created by Spring itself ,using ConditionalOnMissingBean annotations
-     */
-
-    /**
-     * When we use ollama-ai package, ChatModel bean is configured by default with Mistral model
-     * our Task is to use ChatClient bean to interact with the llm model.
-     */
-
-
     /**
      * A Simple Get API with a prompt from user
      * @param input
      * @return
      */
-    @GetMapping("/chat")
+    @GetMapping(value = "/chat", produces = MediaType.TEXT_PLAIN_VALUE)
     public String getChat(@RequestParam("message") String input) {
 
         return chatClient
-                .prompt(input)
+                .prompt()
+                .user(input)
                 .call()
                 .content();
     }
