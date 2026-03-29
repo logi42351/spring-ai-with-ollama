6c8f6bf02f8ba0fa12edc631b1a09c3773732086
Logesh R <logi42351@gmail.com>
2026-03-29 22:39:21 +0530

    streaming the Response vs waiting for full response

--- src/main/java/com/aiservice/backend/controller/ChatController.java
+++ src/main/java/com/aiservice/backend/controller/ChatController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 import java.util.Map;
 
@@ -26,13 +27,13 @@ public class ChatController {
      * will be used in User Prompt
      */
 
-    @Value("classpath:/promptTemplates/UserPrompt.st")
+    @Value("classpath:promptTemplates/UserPrompt.st")
     private Resource userTemplate;
 
     /**
      * Prompt Stuffing for system , that is giving the answer book before looking into the question
      */
-    @Value("classpath:/promptTemplates/SystemPrompt.st")
+    @Value("classpath:promptTemplates/SystemPrompt.st")
     private Resource systemTemplate;
 
 
@@ -62,6 +63,38 @@ public class ChatController {
                 )
                 .call()
                 .content();
+    }
+
+    /**
+     * Streaming of Response from llm model , whenever llm created a word in response , it is transferred then and there
+     *
+     * @param explanation
+     * @param user
+     * @param topic
+     * @return
+     */
+    @GetMapping(value = "/stream")
+    public Flux<String> getStreaming(
+            @RequestParam("explanation") String explanation,
+            @RequestParam("user") String user,
+            @RequestParam("topic") String topic
+    ) {
+
+        return chatClient
+                .prompt()
+                .system(systemTemplate)
+                .user(
+                        promptUserSpec -> promptUserSpec.text(userTemplate)
+                                .params(
+                                        Map.of(
+                                                "explanation", explanation,
+                                                "user",user, "topic",topic
+                                        )
+                                )
+
+                )
+                .stream()    // instead of call() we replace it with a stream() -> returns StreamResponseSpec
+                .content();  // Returns Flux<String>
     }
 
 }
